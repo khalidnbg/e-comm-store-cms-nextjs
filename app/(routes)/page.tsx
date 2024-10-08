dc5c90100a2225b54be837632cd3b20745e45b2e
khalidnbg <khalidnabgaoui4@gmail.com>
2024-10-08 16:09:02 +0100

    add a billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import getBillboards from "@/actions/get-billboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboards = await getBillboards("66fda2b68cbd505786c8de1a");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboards} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
