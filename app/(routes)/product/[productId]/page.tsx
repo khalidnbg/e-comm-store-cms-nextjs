06fc7bbff4e7c0e2f12a5dc9fb7e5c369bad9a6e
khalidnbg <khalidnabgaoui4@gmail.com>
2024-10-08 22:20:07 +0100

    add gallery of images for the individual product

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
+import Gallery from "@/components/gallery";
 
 interface ProductPageProps {
   params: {
@@ -17,15 +18,13 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
     categoryId: product?.category?.id,
   });
 
-  console.log(suggestedProduct);
-
   return (
     <div className="bg-white">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             {/* gallery */}
-            <div>Gallery</div>
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               {/* info */}
               Info
