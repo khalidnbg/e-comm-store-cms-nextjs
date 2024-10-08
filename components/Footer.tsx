9ee5f7343d621c893e2a1355835a633f16eacf8a
khalidnbg <khalidnabgaoui4@gmail.com>
2024-10-08 12:19:39 +0100

    setup project

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sm text-black">
+          &copy; 2024 NbgStore, Inc, All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
